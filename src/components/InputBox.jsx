29c18115a799450e1a438a428e788a6dd5128b78
SanketRankhambe <sanketrankhambe143@gmail.com>
2025-02-12 22:31:01 +0530

    Done

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -12,13 +12,25 @@ function InputBox({
 
     className = ""
 }) {
+
+    // console.log(label,
+    //     amount,
+    //     onAmountChange,
+    //     onCurrencyChange,
+    //     currencyOptions = [],
+    //     selectCurrency = "usd",
+    //     amountDisabled = false,
+    //     curruncuDisabled = false,)
+    const amountInputId = useId();
+
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className='w-1/2'>
-                <label className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
-                <imput
+                <input
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -3,8 +3,20 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({})
 
+    function formatDate() {
+        var d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+
     useEffect(() => {
-        let url = `https://2024-03-06.currency-api.pages.dev/v1/currencies/${currency}.json`
+        let url = `https://${formatDate()}.currency-api.pages.dev/v1/currencies/${currency}.json`
         fetch(url)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
